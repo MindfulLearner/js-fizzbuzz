8a3832126c64eb0368118534ca117aa158ecc2a6
MindfulLearner <dimajosh1009@gmail.com>
2024-07-15 18:33:03 +0000

    init

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+// scriptsssssssssssss
+
+for (let i = 0; i < 99; i++) {
+	if (i % 3 == 0) {
+		if (i % 5 == 0) {
+			log.console("divisibile sia di 3 e 5");
+		} else {
+			log.console("divisibile di 3");
+		}
+	} else if (i % 5 == 0) {
+		log.console("divisibile di 5");
+	} else {
+		log.console("solo numero non divisibile ne di 5 e ne di 3 solo numero");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
